9556bf17de1f677585f9d5d253a9adba27f0db2f
Joud El-Shawa <jelshawa@gmail.com>
2025-04-11 13:36:11 +0000

    added time tracking and readme for report num

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-import Hermes
 import os
+import Hermes
 from Utils.Helpers import *
+import time
+import shutil
 
-REPORT_NUM = 2
+REPORT_NUM = 6
 PATH_DATA = "Data/"
 PATH_EXAMPLE = ""
+OUTPUT_DIR = "Temp/"
 
-if not os.path.exists("Temp/"):
-    os.mkdir("Temp/")
+if os.path.exists(OUTPUT_DIR): # clear the folder up before we start running 
+    shutil.rmtree(OUTPUT_DIR)
+os.mkdir(OUTPUT_DIR)
+
+# document report number in readme so we know what the results correspond to
+readme_path = os.path.join(OUTPUT_DIR, "README.txt")
+with open(readme_path, "w") as f:
+    f.write(f"REPORT_NUM: {REPORT_NUM}\n")
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     print("\n")
     print("#"*60)
     print(" "*20 + "TESTING: MAIN")
     print("#"*60)
     hermes = Hermes.HermesAgenticSystem()
+
     if (REPORT_NUM != -1):
+
         PATH_EXAMPLE = PATH_DATA + str(REPORT_NUM) + "/"
         unstructuredReport = readUnstructuredReport(PATH_EXAMPLE)
         print(f"Report Number {REPORT_NUM}:")
@@ -27,4 +40,6 @@ if __name__ == "__main__":
     print("#"*60)
     print(" "*28 + "END")
     print("#"*60)
+    end_time = time.time()
+    print(f"total time taken to run Hermes: {end_time - start_time:.2f} seconds")
         
